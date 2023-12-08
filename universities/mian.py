f49386d6017cfdb66a0cd06189670eee9bc0fa84
Ronalds Plostnieks <ronalds.plostnieks@gmail.com>
2023-12-08 15:14:10 +0200

    majasdarbs

--- universities/mian.py
+++
@@ -1,6 +0,0 @@
-import requests as rq
-data = rq.get('http://universities.hipolabs.com/search?country=latvia')
-universities = data.json()
-sorted(universities["name"])
-for value in universities:
-    print(value["name"])
--- universities/wordcounter.py
+++ universities/wordcounter.py
@@ -5,21 +5,25 @@ with open("teksts.txt", "r", encoding="utf8") as file:
     for line in file:
         for word in line.split():
             word = word.strip("[$@&().,!?:%-+;\n\"")
-            test_data.append(word.lower())
+            if len(word) > 3:
+                test_data.append(word.lower())
 
-for name in test_data:
-    if name in counting_dict.keys():
-        counting_dict[name] += 1
+
+# for name in test_data: 
+#     name_key = name[:4]
+#     if name_key in counting_dict.keys(): 
+#         counting_dict[name_key] += 1 
+#     else: 
+#         counting_dict[name_key] = 1
+
+for name in test_data: 
+    if name in counting_dict.keys(): 
+        counting_dict[name] += 1 
     else: 
         counting_dict[name] = 1
 
-biggest = ""
-biggest_value = 0
-
 for key, value in counting_dict.items():
-    if value > biggest_value:
-        biggest_value = value
-        biggest = key
+    print(key, value)
 
-
-print(biggest, biggest_value)
+scounting_dict = sorted(counting_dict.items(), key=lambda x: x[1], reverse=True)[:5]
+print(scounting_dict)
