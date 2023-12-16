36c6863b1ab066a610ad9aae554f5ba6f3bdfa4b
Ronalds Plostnieks <ronalds.plostnieks@gmail.com>
2023-12-16 11:58:43 +0200

    real code

--- chucknorris/main.js
+++ chucknorris/main.js
@@ -1,7 +1,17 @@
+var categoriesArray = ["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"];
+var dropdown = document.getElementById("categorieSelect");
+
+categoriesArray.forEach(function(category, i) {
+    var option = document.createElement("option");
+    option.value = category;
+    option.text = category;
+    option.id = i;
+    dropdown.add(option);
+});
+
 async function newJoke() {
-    const jokes = await fetch(`https://api.chucknorris.io/jokes/random`);
+    var selectedCategory = dropdown.options[dropdown.selectedIndex].value;
+    const jokes = await fetch(`https://api.chucknorris.io/jokes/random?category=${selectedCategory}`);
     data = await jokes.json();
     document.getElementById('jokePlace').innerHTML = data.value;
-    // document.getElementById('jokePlace').innerHTML = JSON.stringify(data);
 }
-
